e785d9eada3827b97c438ad3592aedb0a0ed0004
Sean Dan <seandan@seans-mbp.myfiosgateway.com>
2021-01-25 20:32:11 -0500

    additional cleanup

--- src/routes/RegisterPage.js
+++ src/routes/RegisterPage.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
-//import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-//import Box from "@material-ui/core/Box";
-//import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -48,7 +45,7 @@ const RegisterPage = () => {
     event.preventDefault();
     await axios.post("http://localhost:5001/register", account);
     setTimeout(() => {
-      history.push("/home");
+      history.push("/login");
     }, 1500);
   };
 
@@ -56,9 +53,6 @@ const RegisterPage = () => {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        {/* <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar> */}
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
